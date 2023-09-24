8a2b29bb0f8c447801273355581abec8ca763e61
Minh Nguyen <10432272-tmnguyen1403@users.noreply.gitlab.com>
2023-09-24 02:59:41 -0400

    compare odd_even sort parallel with std::sort

---
+++ openmp/odd_even_transposition_sort.cpp
@@ -0,0 +1,59 @@
+/*
+Algorithm is copied from Chapter 5, p. 248
+Pacheco, P. S. An introduction to parallel programming 2nd.
+*/
+#include <iostream>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+#include <string>
+#include <cstdio>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+int main(int argc, char* argv[]) {
+    int N = std::stoi(argv[1]);
+    //std::cout << N << std::endl;
+    int thread_count = std::stoi(argv[2]);
+    std::vector<int> a{};
+    a.resize(N);
+    std::iota(a.begin(), a.end(), 1);
+    int n = a.size();
+    if (thread_count == 1) {
+        std::sort(a.begin(), a.end());
+        std::cout << "Complete quick sort\n";
+        // for(const auto& value : a) {
+        //     std::cout << value << " ";
+        // }
+        // std::cout << std::endl;
+        return 1;
+    }
+    #pragma omp parallel num_threads(thread_count) \
+        default(none) shared(a, n)
+    {
+        for (int phase = 0; phase < n; ++phase) {
+            if (phase % 2 == 0) {
+                #pragma omp for
+                for (int i = 1; i < n; i+=2) {
+                    if (a[i-1] > a[i]) {
+                        std::swap(a[i-1], a[i]);
+                    }
+                }
+            }
+            else {
+                #pragma omp for
+                for (int i = 1; i < n -1; i+=2) {
+                    if (a[i] > a[i+1]) {
+                        std::swap(a[i], a[i+1]);
+                    }
+                }
+            }
+        }
+    }
+    std::cout << "Complete odd_even_transposition sort\n";
+    // for(const auto& value : a) {
+    //     std::cout << value << " ";
+    // }
+    std::cout << std::endl;
+}
---
+++ openmp/sort_compare.md
@@ -0,0 +1,6 @@
+## Execution time table
+| N  | std::sort | odd_even_transposition_sort 4thread | 8thread | 12thread |
+| -- | --------- | ----------------------------------- | ------- | -------- |
+| 123'000 | 0.01s | 34.21s | 68.18s | 74.08s |
+
+| 123'000'000 | 3.13s | forever | forever| forever |
